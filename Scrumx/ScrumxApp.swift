9b804f1a0c45ad11e60c4a0bf16e1f65bb523649
j1n.dev <jan.francksen@gmail.com>
2023-11-27 23:03:21 +0100

    Initial Commit

---
+++ Scrumx/ScrumxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumxApp.swift
+//  Scrumx
+//
+//  Created by Jan Francksen on 27.11.23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
