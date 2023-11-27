c684f5cdd8c6f773c5c2634b093d89e03b85df76
j1n.dev <jan.francksen@gmail.com>
2023-11-27 23:26:59 +0100

    feat(meeting): add basic screen layout

---
+++ Scrumx/MeetingView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  Scrumx
+//
+//  Created by Jan Francksen on 27.11.23.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds elapsed").font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds remaining").font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
--- Scrumx/ScrumxApp.swift
+++ Scrumx/ScrumxApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ScrumxApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MeetingView()
         }
     }
 }
